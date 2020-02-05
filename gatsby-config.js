74eda3b1db8633b9b5ea2877b35d0efb527fb95f
JEHYUNLIM <ljehyun@gmail.com>
2020-02-05 13:28:21 +0900

    Init Jehyun's Blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,14 +5,14 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
+    // `gatsby-plugin-react-helmet`,
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -35,8 +35,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/src/markdown_files`
+        name: `pages`,
+        path: `${__dirname}/src/pages`
       },
     },
     // for transform Markdown files
