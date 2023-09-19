411bc0748805d33b33a584b40a81c2cbf81b1b05
JeHyun Lim <jehyunlim@JeHyuns-MacBook-Pro.local>
2023-09-19 11:46:29 +0900

    build

---
+++ build/precache-manifest.01a492f17f53e18af1f21a5d7b2b95a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ce6ab29537a30597550",
+    "url": "/static/css/main.3b835488.chunk.css"
+  },
+  {
+    "revision": "3ce6ab29537a30597550",
+    "url": "/static/js/main.3ce6ab29.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "06efcf0fd2ac860d3816",
+    "url": "/static/js/2.06efcf0f.chunk.js"
+  },
+  {
+    "revision": "439225c8a17b56244b87",
+    "url": "/static/js/runtime~main.439225c8.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b948573b76b4c06c19e68c5c23ff95b6",
+    "url": "/index.html"
+  }
+];
